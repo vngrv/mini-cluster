5b08472bf9bf1b71ee784834ce71c026fe2f1a4a
Димундель <dimundel@MacBook-Pro-Mac.local>
2020-09-25 23:50:10 +0300

    Release: messaging without clusters

--- index.js
+++ index.js
@@ -11,28 +11,67 @@ app.use((request, response, next) => {
 
     if(cluster.isWorker) {
         console.log(`Worker ${cluster.worker.id} handle request`);
-    };
+    } 
 
     next();
 });
 
 app.get('/', (request, response) => {
-    // response.send('Cluster mode.');
-    response.send(`Worker ${cluster.worker.id} handle request`);
+    response.send(`Worker ${cluster.worker.id} handle request`);    
 });
 
 if(cluster.isMaster) {
-    let cpus = os.cpus().length;
 
-    for(let i = 0; i < cpus; i++) {
-        cluster.fork();
+    for(let i = 0; i < 2; i++) {
+        let worker = cluster.fork();
+        // Получать сообщения от этого воркера и обрабатывать их в главном процессе.
+        recievedToMaster(worker);
+        // Отправьте сообщение от главного процесса воркеру.
+        sendToWorker(worker);        
     }
 
     cluster.on('exit', (worker, code) => {
-        console.log(`Worker ${worker.id} finished. Exit code: ${code}`);
-
-        app.listen(PORT, HOST, () => console.log(`Worker ${cluster.worker.id} launched`));
+        app.listen(PORT, HOST, () => console.log(`-- Worker ${cluster.worker.id} launched`));
     });
+
 } else {
-    app.listen(PORT, HOST, () => console.log(`Worker ${cluster.worker.id} launched`));
+    console.log('\n- Worker ' + process.pid + ' has started.');
+    // Отправить сообщение главному процессу.
+    sendToMaster();
+    // Получать сообщения от главного процесса.
+    recievedToWorker();
+    
+    app.listen(PORT, HOST, () => console.log(`- Worker ${cluster.worker.id} launched`));
+}
+
+function generateData() {
+    return Math.random(100);
+}
+
+function recievedToMaster(worker) {
+    return worker.on('message', function(msg) {
+        console.log(`\n-- Master (${process.pid})\n-- received message from worker (${worker.process.pid}):\n  `, msg, '\n')
+    });
+}
+function sendToWorker(worker) {
+    let message = {
+        masterId: process.pid,
+        toWorker: worker.id,
+        message: generateData()
+    };
+
+    console.log(`Generate new message from master node`, message.message)
+
+    return worker.send(message);
+}
+
+
+function sendToMaster() {
+    process.send({message: 'This is from worker ' + process.pid + '.'})
+}
+
+function recievedToWorker() {
+    process.on('message', function(msg) {
+        console.log('- Worker ' + process.pid + ' received message from master ' + msg.masterId, msg.message);
+    });
 }
